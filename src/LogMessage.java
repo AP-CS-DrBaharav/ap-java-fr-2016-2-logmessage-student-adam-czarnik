3d3a016a75292b9268fad5bcab2713d430afbe51
adam-czarnik <aczarnik@2018.kehillah.org>
2018-05-13 13:54:03 -0700

    I can't run any of this code because when I try to compile to run it has some error about a unicode characters or something... so I can't test this at all

--- src/LogMessage.java
+++ src/LogMessage.java
@@ -5,32 +5,33 @@ public class LogMessage
 
   /* Part (a) */
 
-  public LogMessage(String message)
-
-          
-          
-          
-          
-          
-          
-          
-          
+  public LogMessage(String message) {
+      int idx = 0;
+      for (int i=0; i<message.length()-1; ++i) {
+          if (message.substring(i,i+1).compareTo(":") == 0)
+              idx = i;
+      }
+         machineId = message.substring(0,idx);
+         description = message.substring(idx+1);
+  }
           
   /* Part (b) */
 
-  public boolean containsWord(String keyword)
+  public boolean containsWord(String keyword) {
 
-          
-          
-          
-          
-  
-          
-          
-          
-          
-          
-          
+      for (int i=0; i<description.length()-keyword.length(); ++i) {
+          if (description.substring(i,i+keyword.length()).compareTo(keyword)==0) {
+              if (i==0 || description.substring(i-1,i).compareTo(" ")==0) {
+                  return true;
+              } else if (i==description.length()-keyword.length()-1 || description.substring(i+keyword.length(),i+1+keyword.length()).compareTo(" ")==0) {
+                  return true;
+              } else {
+                  return false;
+              }
+          }
+      }
+        return false;  
+  }    
 
   public String getMachineId()
   { return machineId; }
